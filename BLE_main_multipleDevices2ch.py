48759d260abdb0065a0d4875ce523a687f47607c
Eduard Buss <eduard.buss@uni-konstanz.de>
2025-06-04 11:39:37 +0200

    v2

--- BLE_main_multipleDevices2ch.py
+++ BLE_main_multipleDevices2ch.py
@@ -1,13 +1,12 @@
-import argparse
 import asyncio
 import logging
+from datetime import datetime
 from functools import partial
-from typing import Optional
+import argparse
 
-from bleak import BleakClient, BleakScanner
-from bleak.backends.device import BLEDevice
-from bleak.backends.characteristic import BleakGATTCharacteristic
+from bleak import BleakClient, BleakScanner, BLEDevice
 from MyBleakClient2ch import MyBleakClient
+
 
 try:
     from mu_interface.Utilities.log_formatter import setup_logger, log_DBGX
@@ -24,7 +23,7 @@ except ImportError:
 NOTIFICATION_UUID = "5a3b0203-f6dd-4c45-b31f-e89c05ae3390"
 
 
-async def discoverNodes(name) -> Optional[BLEDevice]:
+async def discoverNodes(name):  # -> BLEDevice | None:
     """
     Discovering devices and return object of device
     """
@@ -37,39 +36,42 @@ async def discoverNodes(name) -> Optional[BLEDevice]:
     return None
 
 
-async def conectToNode(lock, name, OB_activate): # -> tuple[None, None] | tuple[BLEDevice, MyBleakClient]:
+async def conectToNode(lock, name, OB_activate):  # -> BleakClient | None:
     """
     Finds device with device name and connects to it
     """
     async with lock:
         logging.info(f"Connecting to device with name: {name}")
         device = await BleakScanner.find_device_by_name(name)
-        if not device:
-            logging.error(f"FAILED connection to device with name: {name}")
-            return None, None
+    if not device:
+        logging.error(f"FAILED connection to device with name: {name}")
+        return None, None
 
-        client = MyBleakClient(device, OB_active = OB_activate, disconnected_callback=disconnected_callback)
-        await client.connect()
-        if client.is_connected:
-            logging.info(f"Connected to device with name: {name}")
-            return device, client
-        else:
-            logging.error(f"FAILED connection to device with name: {name}")
-            return None, None
+    client = MyBleakClient(device, OB_active = OB_activate, disconnected_callback=disconnected_callback)
+    # Modification: Check if the client is already connected TODO: new
+    if client.is_connected:
+        logging.info(f"Already connected to {name}")
+        return device, client
+
+    await client.connect()
+    if client.is_connected:
+        logging.info(f"Connected to device with name: {name}")
+        return device, client
+    else:
+        logging.error(f"FAILED connection to device with name: {name}")
+        return None, None
 
 
 async def notification_callback_handler(sender, data):
     print("Read:", int.from_bytes(data, "big"))
 
 
-async def my_notification_callback_with_client_input(client, sender: BleakGATTCharacteristic, data: bytearray):
+async def my_notification_callback_with_client_input(client, sender: int, data: bytearray):
     """Notification callback with client awareness"""
     singl_val = [data[i : i + 3] for i in range(0, len(data), 3)]
-    # data = int.from_bytes(data, "big")
+    #data = int.from_bytes(data, "big")
     real_val = [int.from_bytes(d, "big") for d in singl_val]
-
-    logging.log(log_DBGX, f"Data: {real_val}, {[getVolt(d) for d in real_val]}")
-    name = client.device.name
+    #name = client.device.name
     await client.write2csv_io(real_val, 200)
 
 
@@ -96,43 +98,38 @@ async def NotificationRoutine(lock, device_name, OB_activate):
             try:
                 connection_task = asyncio.create_task(conectToNode(lock, device_name, OB_activate))
                 device, client = await connection_task
-                # await asyncio.sleep(1)
+                await asyncio.sleep(2)  # Short delay before attempting notifications TODO: new
             except Exception as e:
                 logging.error(f"{device_name} connection error: {e}")
-                await asyncio.sleep(1)  # Wait a bit before trying again.
-
-        if client is not None:
-            try:
-                await client.start_notify(NOTIFICATION_UUID, partial(my_notification_callback_with_client_input, client))
-                while (client.is_connected):
-                    await asyncio.sleep(1)
-            except Exception as e:
-                logging.error(f"Error has occured: {e}")
-                device = None
-                client = None
-                # await client.stop_notify(NOTIFICATION_UUID)
-                ##print("error with", str(e))   <- e is not printed, probably bleak specific
-                ##if not device_object.is_connected:
-                ##connection_task = asyncio.create_task(conectToNode(lock,device_name))
-                ##device, client = await connection_task
+                await asyncio.sleep(5)  # Add delay before retrying   TODO: new
+                continue
+        try:
+            await client.start_notify(NOTIFICATION_UUID, partial(my_notification_callback_with_client_input, client))
+            while (client.is_connected):
+                await asyncio.sleep(1)
+        except Exception as e:
+            logging.error(f"Error has occured: {e}")
+            device = None
+            client = None
 
 
 async def main():
-    parser = argparse.ArgumentParser(description="Activate Orange Box integeration")
-    parser.add_argument("--OB_activate", action="store_true", help="Activate Orange Box integeration")
-    parser.add_argument("--debug", action="store_true", help="Activate debug mode")
+    parser = argparse.ArgumentParser(
+        description="Activate Orange Box integeration"
+    )
+    parser.add_argument("--OB_activate", required=True, type=int)
     args = parser.parse_args()
 
-    logging.addLevelName(log_DBGX, "DEBUG")
-    setup_logger("BLE Interface", log_DBGX if args.debug else logging.INFO)
-
-    # try:
-    device_names = ["P1"]  # ['P1','P2','P3']#,'P4','P5','P6','P7']
+    device_names = ['P1']
     lock = asyncio.Lock()
-    device = await asyncio.gather(*(NotificationRoutine(lock, name, args.OB_activate) for name in device_names))
+    device = await asyncio.gather(*(NotificationRoutine(lock,name,args.OB_activate) for name in device_names))
+
 
 
 if __name__ == "__main__":
+    logging.addLevelName(log_DBGX, "DEBUG")
+    setup_logger("BLE Interface", log_DBGX)
+    
     asyncio.run(main())
     """
     for i in range(100):
--- MyBleakClient2ch.py
+++ MyBleakClient2ch.py
@@ -1,11 +1,14 @@
+import csv
 import logging
 import socket
 from datetime import datetime, timedelta
 from pathlib import Path
 
 import aiofiles
+import numpy
 from aiocsv import AsyncDictReader, AsyncDictWriter, AsyncReader, AsyncWriter
 from bleak import BleakClient
+
 
 
 try:
@@ -17,25 +20,8 @@ except ImportError:
         data = '%Y-%m-%d %H:%M:%S:%f'
 
 
-def array_split(array, n):
-    """Split an array into n equal parts."""
-    split, rem = divmod(len(array), n)
-    if rem != 0:
-        raise ValueError(f"Array length {len(array)} is not divisible by {n}.")
-
-    list_of_arrays = []
-    for i in range(n):
-        list_of_arrays.append(array[i * split:(i + 1) * split])
-
-    return list_of_arrays
-
-
-def mean(array):
-    """Calculate the mean of an array."""
-    return sum(array) / len(array)
-
 class MyBleakClient(BleakClient):
-    def __init__(self, device, OB_active, **kwargs):
+    def __init__(self, device,OB_active, **kwargs, ):
         super().__init__(device, **kwargs)
         self.device = device
 
@@ -45,12 +31,10 @@ class MyBleakClient(BleakClient):
                 experiment_number = int(f.read())
             self.experiment_name = f"{socket.gethostname()}_{experiment_number}"
             self.file_path = Path.home() / 'measurements' / self.experiment_name / 'BLE' / self.device.name
-            self.file_name_prefix = f"{self.experiment_name}_{self.device.name}"
+            self.file_name = f"{self.experiment_name}_{self.device.name}_{datetime.now().strftime(TimeFormat.file)}.csv"
         else:
-            self.file_path = Path('/home/pi/Desktop/measurment', self.device.name)
-            self.file_name_prefix = f"{self.device.name}"
-        self.file_name = f"{self.file_name_prefix}_{datetime.now().strftime(TimeFormat.file)}.csv"
-        self.avg_file_name = f"ZZ_AVG_PLOTTING_{self.device.name}_IGNORE_ME.csv"
+            self.file_path = Path('/home/pi/Desktop/measurement', self.device.name)
+            self.file_name = f"{self.device.name}_{datetime.now().strftime(TimeFormat.file)}.csv"
 
         ## CSV - new
         Path(self.file_path).mkdir(parents=True, exist_ok=True)  # make new directory
@@ -60,86 +44,65 @@ class MyBleakClient(BleakClient):
         self.header = ["datetime", "CH1", "CH2"]  #'differential_potential']
         self.last_csv_time = datetime.now()
 
-        # Stuff for averaging high frequency data to lower frequency for live plotting
-        self.avg_buffer_len = 10
-        self.avg_buffer_idx = 0
-        self.avg_buffer = [[0.0] * self.avg_buffer_len, [0.0] * self.avg_buffer_len]
-        self.avg_lines_to_copy = 120 * 60  # 120 minutes of data
-        self.avg_first_write = True
-
+        self.first_value = True
         self.current_time = None
         self.last_time = None
+        if OB_active:
+            self.status_dir = Path("/home/rock/OrangeBox/status/measuring")
+            self.status_dir.mkdir(parents=True, exist_ok=True)
+            self.csvfile1 = open(self.status_dir / self.device.name, 'a+')
+            self.csvfile1.close()
 
     async def write2csv_io(self, data, freq):
+        # try:
         # data is the array of every data (3 bytes each) so 66 entries
         # first 33 are ch0, last 33 are ch1, need to split
-        temp = array_split(data, 2)
+        temp = numpy.array_split(data, 2)
         data0 = temp[0]
         data1 = temp[1]
 
-        # Create new file every 12 hours
         self.current_time = datetime.now()
-        if self.current_time.hour in {0,12} and self.current_time.hour != self.last_csv_time.hour:
+        if self.current_time.hour in {0,12} and self.current_time.hour != self.last_csv_time.hour:  # create new file every 12 hours
+            # self.csvfile.close()
             self.last_csv_time = datetime.now()
-            self.file_name = f"{self.file_name_prefix}_{datetime.now().strftime(TimeFormat.file)}.csv"
-            self.avg_first_write = True
+            self.file_name = f"{self.device.name}_{datetime.now().strftime(TimeFormat.file)}.csv"
             logging.info("Creating a new csv file.")
-            self.last_time = None
+            self.first_value = True
 
+        # self.csvfile = open(self.file_path + self.file_name, 'a+')
         async with aiofiles.open(self.file_path / self.file_name, mode="a+") as f:
-            csvwriter = AsyncWriter(f)
+            self.csvwriter = AsyncWriter(f)
             # times gets the time for every data entry
             # ch0 and ch1 should be at the same time, so only need to use one of them for timing
             times = []
-
-            if self.last_time is None:
-                await csvwriter.writerow(self.header)
-                time_delta = timedelta(milliseconds=1 / freq * 1000)
+            # TODO: condense if/else
+            if self.first_value:
+                await self.csvwriter.writerow(self.header)
+                # print("header written")
+                t_delta = 1 / freq * 1000
+                time_delta = timedelta(milliseconds=t_delta)
+                # calculating the time for each data entry starting from the last one
+                # only need for 1 channel since they are measured at same time
+                for i in range(len(data0)):
+                    times_string = (self.current_time - i*time_delta).strftime(TimeFormat.data)
+                    times.append(times_string)
+                times.reverse()
+                # ^ need to reverse because calcuated times backwards
+                combined_list = [[x, y, z] for x, y, z in zip(times, data0, data1)]
+                self.first_value = False
+                self.last_time = self.current_time
             else:
-                time_delta = (self.current_time - self.last_time) / len(data0)
+                # print(self.current_time-self.last_time, self.current_time, self.last_time)
+                t_delta = (self.current_time - self.last_time) / len(data0)
+                # time_delta = timedelta(t_delta)
+                for i in range(len(data0)):
+                    times_string = (self.current_time - i*t_delta).strftime(TimeFormat.data)
+                    times.append(times_string)
+                times.reverse()
+                combined_list = [[x, y, z] for x, y, z in zip(times, data0, data1)]
+                self.last_time = self.current_time
+            data4csv = combined_list
+            #print(data4csv)
 
-            # calculating the time for each data entry starting from the last one
-            # only need for 1 channel since they are measured at same time
-            for i in range(len(data0)):
-                times_string = (self.current_time - i*time_delta).strftime(TimeFormat.data)
-                times.append(times_string)
-
-            times.reverse()  # need to reverse because calcuated times backwards
-            combined_list = [[x, y, z] for x, y, z in zip(times, data0, data1)]
-            self.last_time = self.current_time
-
-            await csvwriter.writerows(combined_list)
-        """
-        # Averaging for live plotting
-        self.avg_buffer[0][self.avg_buffer_idx] = mean(data0)
-        self.avg_buffer[1][self.avg_buffer_idx] = mean(data1)
-        self.avg_buffer_idx += 1
-
-        try:
-            if self.avg_first_write:
-                await self.reset_avarage_file()
-                self.avg_first_write = False
-
-            if self.avg_buffer_idx == self.avg_buffer_len:
-                self.avg_buffer_idx = 0
-                async with aiofiles.open(self.file_path / self.avg_file_name, mode="a+") as f:
-                    avg_csvwriter = AsyncWriter(f)
-                    await avg_csvwriter.writerow([self.current_time.strftime(TimeFormat.data),
-                                                round(mean(self.avg_buffer[0])),
-                                                round(mean(self.avg_buffer[1]))])
-        except Exception as e:
-            logging.error(f"Error in writing to or resetting the average file: {e}")
-
-    async def reset_avarage_file(self):
-        content = []
-        if (self.file_path / self.avg_file_name).exists():
-            async with aiofiles.open(self.file_path / self.avg_file_name, 'r') as f:
-                async for row in AsyncReader(f):
-                    if row != self.header:
-                        content.append(row)
-
-        async with aiofiles.open(self.file_path / self.avg_file_name, 'w') as f:
-            avg_csvwriter = AsyncWriter(f)
-            await avg_csvwriter.writerow(self.header)
-            await avg_csvwriter.writerows(content[-self.avg_lines_to_copy:])
-        """
+            await self.csvwriter.writerows(data4csv)
+            # self.csvfile.close()
